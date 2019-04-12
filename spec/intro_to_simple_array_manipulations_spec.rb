bed6fc95b4b50f9918d23da78b1a19f0b407d615
Kellye Greene <kellyecodes@gmail.com>
2019-04-12 15:28:03 -0400

    Simplifies tests

--- spec/intro_to_simple_array_manipulations_spec.rb
+++ spec/intro_to_simple_array_manipulations_spec.rb
@@ -4,18 +4,18 @@ require './lib/intro_to_simple_array_manipulations.rb'
 
 describe "#using_push" do 
   it "takes in two arguments, an array and a string and adds that string to the end of the array using the push method" do 
-    countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-    next_country = "Niger"
-    updated_array = using_push(countries_in_western_africa, next_country)
-    expect(updated_array.last).to eq(next_country)
+    colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+    next_color = "violet"
+    updated_array = using_push(colors_in_the_rainbow, next_color)
+    expect(updated_array.last).to eq(next_color)
     end
 end
 
 describe "#using_unshift" do 
   before(:each) do 
-    neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-    @new_neighborhood = "Brooklyn Heights"
-    @updated_array = using_unshift(neighborhoods_in_northwest_brooklyn, @new_neighborhood)
+    bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+    @new_neighborhood = "Staten Island"
+    @updated_array = using_unshift(bouroughs_in_nyc, @new_neighborhood)
   end
   
   it "takes in two arguments, an array and a string and adds that string to the front of the array using the unshift method" do
@@ -23,35 +23,35 @@ describe "#using_unshift" do
   end
 
   it "increases the length of the array" do 
-    expect(@updated_array.size).to eq(6)
+    expect(@updated_array.size).to eq(5)
   end
 end
 
 describe "#using_pop" do
   before(:each) do 
-    @great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-    @deleted_string = using_pop(@great_hits_of_the_nineties)
+    @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+    @deleted_string = using_pop(@continents)
   end
   it "takes in argument of an array and uses the pop method to remove the last element from the array and return that element" do 
-    expect(@deleted_string).to eq("Losing My Religion")
+    expect(@deleted_string).to eq("Antarctica")
   end
 
   it "decreases the length of the array by 1" do 
-    expect(@great_hits_of_the_nineties.size).to eq(5)
+    expect(@continents.size).to eq(6)
   end
 end
 
 describe "#pop_with_args" do
   before(:each) do 
-    @chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
-    @chars_arya_killed = pop_with_args(@chars_in_game_of_thrones)
+    @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+    @small_dogs = pop_with_args(@dog_breeds)
   end 
   it "takes in an argument of an array and uses the pop method with an argument of 2 to remove the last two array items and return them" do 
-    expect(@chars_arya_killed).to eq(["Stable Boy", "Sandor Clegane"])
+    expect(@small_dogs).to eq(["Chihuahua", "Shiba Inu"])
   end 
 
   it "decreases the length of the array by 2" do
-    expect(@chars_in_game_of_thrones.size).to eq(2) 
+    expect(@dog_breeds.size).to eq(2)
   end
 end
 
@@ -87,11 +87,11 @@ end
 describe "#using_concat" do 
   before(:each) do 
     @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-    more_favs = ["mario kart", "flatiron school"]
+    more_favs = ["sports cars", "flatiron school"]
     @all_my_favs = using_concat(@my_favorite_things, more_favs)
   end
    it "takes in two arguments of two different arrays and uses the concat method to add the contents of the second array to the first" do 
-    expect(@all_my_favs).to eq(["raindrops on roses", "whiskers on kittens", "mario kart", "flatiron school"])
+    expect(@all_my_favs).to eq(["raindrops on roses", "whiskers on kittens", "sports cars", "flatiron school"])
    end
 
    it "increases the length of the first array" do 
@@ -101,13 +101,13 @@ end
 
 describe "#using_insert" do 
   before(:each) do 
-    @list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
-    @another_esoteric_language = "Malbolge"
-    @new_array = using_insert(@list_of_esoteric_programming_languages, @another_esoteric_language)
+    @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+    @another_language = "Python"
+    @new_array = using_insert(@list_of_programming_languages, @another_language)
   end
 
   it "takes in two arguments, an array and a new element to be added to the array. it uses the insert method to add the new element to the 4th index of the array" do 
-    expect(@new_array[4]).to eq(@another_esoteric_language)
+    expect(@new_array[4]).to eq(@another_language)
   end
 
   it "increases the length of the array" do 
@@ -117,17 +117,17 @@ end
 
 describe "#using_uniq" do 
   it "takes in an argument of an array and uses the uniq method to remove any duplicate items" do
-    captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
-    new_array = using_uniq(captain_planet_and_the_planeteers)
-    expect(new_array).to eq(["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler"])
+    haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+    new_array = using_uniq(haircuts)
+    expect(new_array).to eq(["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler"])
   end
 end
 
 describe "#using_flatten" do 
   it "takes in an argument of an array that contains other arrays and uses the flatten method to return an array of strings" do 
-     private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
-     flat_array = using_flatten(private_colleges_in_newyork)
-     expect(flat_array).to eq(["New York University", "Manhattan School of Music ", "Columbia University", "The Juilliard School", "Bard College", "Cooper Union"])
+     instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
+     flat_array = using_flatten(instruments)
+     expect(flat_array).to eq(["Saxophone", "Piano", "Trumpet", "Violin", "Drums", "Flute"])
   end
 end
 
@@ -142,8 +142,8 @@ end
 
 describe "#using_delete_at" do 
   it "takes in two arguments, an array and an integer and deletes the element at the index of the array that is equal to that integer" do 
-    famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
+    famous_robots = ["Johnny 5", "R2D2", "Robocop"]
     deleted_robot = using_delete_at(famous_robots, 2)
-    expect(deleted_robot).to eq("Ultron")
+    expect(deleted_robot).to eq("Robocop")
   end
 end
