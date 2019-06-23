87148ffef6d3ca24e4471a262ffab7fe70345a86
yassir bennani <ybennani@yahoo.com>
2019-06-23 22:58:03 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_push(array, string)
+array.push(string)
+end
+
+def using_unshift(array, string)
+
+   array.unshift(string)
+end
+def using_pop(array, string)
+
+   array.pop(string)
+end
+def pop_with_args(array, ,string,string1)
+
+   array.pop(string,string1)
+end
