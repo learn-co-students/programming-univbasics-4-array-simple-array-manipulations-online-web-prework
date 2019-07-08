fde586fc5293e7c113dab97b94f93e2b1aa66779
Brandon Parris <brandonparrisguy@gmail.com>
2019-07-08 19:43:06 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,41 @@
+def using_push(colors_in_the_rainbow, next_color)
+
+return colors_in_the_rainbow.push(next_color)
+end
+def using_unshift(colors_in_the_rainbow, next_color)
+
+return colors_in_the_rainbow.unshift(next_color)
+end
+def using_pop(colors_in_the_rainbow)
+
+return colors_in_the_rainbow.pop
+end
+def pop_with_args(small_dogs)
+
+return small_dogs.pop(2)
+end
+def using_shift(my_favorite_cities)
+
+return my_favorite_cities.shift
+end
+def shift_with_args(ice_cream_brands)
+ice_cream_brands.shift(2)
+end
+def using_concat(my_favorite_things, more_favs)
+  my_favorite_things.concat(more_favs)
+end
+def using_insert(list_of_programming_languages, another_language)
+  list_of_programming_languages.insert(4,another_language)
+end
+def using_uniq(haircuts)
+  haircuts.uniq
+end
+def using_flatten(instruments)
+  instruments.flatten
+end
+def using_delete(instructors, steven)
+  instructors.delete(steven)
+end
+def using_delete_at(famous_robots, robocop)
+  famous_robots.delete_at(2)
+end
