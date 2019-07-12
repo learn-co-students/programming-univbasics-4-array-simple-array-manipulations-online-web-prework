5f222c0d51fb42f2b12ef7c20f847f918ef072b1
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-07-12 13:10:57 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,2 +1,51 @@
-next_color = "violet"
-colors_in_the_rainbow.push(next_color)
+def using_push(array, string) 
+  puts
+array.push(string)
+end
+
+def using_unshift(array,string)
+  puts 
+  array.unshift(string)
+end 
+def using_pop(array)
+  puts 
+  array.pop
+end 
+
+def pop_with_args(array)
+  puts 
+array.pop(2)
+end 
+
+def using_insert(array, element)
+puts array.insert(4,element)
+end
+
+def using_shift(array)
+  puts 
+array.shift
+end 
+
+def using_shift_with_args(array)
+  puts 
+array.shift(1)
+end 
+
+def using_concat(array1, array2)
+  puts 
+array1.concat(array2)
+end 
+def using_uniq(array)
+  puts 
+  array.uniq
+  end
+  def using_flatten(array)
+    puts
+    array.flatten
+  end
+  def using_delete(array, string)
+    puts
+    array.delete(string)
+  end
+  
+  
