c0beed8b2b3db9c141ff849ee499789b0ffbb594
Levi Borland <borland.levi@gmail.com>
2019-07-13 09:10:48 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,14 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop 
+end
+
+def pop_with_args(array)
+  
