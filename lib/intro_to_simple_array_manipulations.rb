148f65e2ddd8a0eac95ca4f535c03ac98a7ab9b2
Renee France <renee5.0@outlook.com>
2019-07-13 02:46:31 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+ def colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+ next_color = "violet"
+ colors_in_the_rainbow.push(next_color)
+ end
+ 
+ def bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+   
+ end
+ 
+ def @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+   
+ end
+ 
+ def  @dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+    @small_dogs = pop_with_args(@dog_breeds)
+  end
+  
+ 
+ 
