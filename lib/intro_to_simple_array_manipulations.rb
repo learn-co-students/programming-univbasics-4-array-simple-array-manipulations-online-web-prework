e45ea1a7ee49fed542d402bf8ae4d7b38cbf8059
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-07-14 22:12:41 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,30 @@
+def using_push (array, string)
+  
+  array.push(string)
+  
+end
+
+def using_unshift (array,string)
+  
+  array.unshift(string)
+  
+end
+
+def using_pop (array)
+  
+  array.pop
+  
+end
+
+def pop_with_args (array)
+  
+  array.pop(2)
+  
+end
+
+def using_shift (array)
+  
+  array.shift
+  
+end
+
