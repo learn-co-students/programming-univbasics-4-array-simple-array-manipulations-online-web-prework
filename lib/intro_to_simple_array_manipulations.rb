347feba956de20f2bee38de4d5f6cfdcc3deea4a
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-07-14 22:23:30 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -28,3 +28,50 @@ def using_shift (array)
   
 end
 
+def shift_with_args (array)
+  
+  array.shift(2)
+  
+end
+
+def using_concat (array1, array2)
+  
+  array1.concat(array2)
+  
+end
+
+def using_insert (array, element)
+  
+  array[4].insert(element)
+  
+end
+
+def using_uniq (array)
+  
+  array.uniq
+  
+end
+
+def using_flattern (array)
+  
+  array.flatten
+  
+end
+
+def pop_with_args (array)
+  
+  array.pop(2)
+  
+end
+
+def using_delete (array, string)
+  
+  array.delete(string)
+  
+end
+
+def using_delete_at (array, interger)
+  
+  array.delete[interger]
+  
+end
