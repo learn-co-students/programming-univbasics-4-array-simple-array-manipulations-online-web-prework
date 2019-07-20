b98d84d06a9f3577542652431eab1f12fe061c7b
Warren Loy Harper II <warrenharper2@att.net>
2019-07-20 21:57:03 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,43 @@
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+next_color = "violet"
+colors_in_the_rainbow.push(next_color)
+end
+bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+new_neighborhood = "Staten Island"
+bouroughs_in_nyc.shift(new_neighborhood)
+end
+bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+new_neighborhood = "Staten Island"
+bouroughs_in_nyc.unshift(new_neighborhood)
+end
+continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+continents.pop
+end
+dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+dog_breeds.pop(2)
+end
+my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+my_favorite_cities.shift
+end
+my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+my_favorite_cities.shift(2)
+my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+more_favs = ["sports cars", "flatiron school"]
+more_favs.concat 
+end
+list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+another_language = "Python"
+another_language.insert
+end
+haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+haircuts.uniq
+end
+instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
+instruments.flatten
+end
+instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+instructors.delete(3)
+end
+famous_robots = ["Johnny 5", "R2D2", "Robocop"]
+famous_robots.delete(2)
+end
