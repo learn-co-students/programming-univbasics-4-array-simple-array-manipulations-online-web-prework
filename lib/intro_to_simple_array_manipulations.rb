f0c9163a1bf10edcd762b4255d29a2049aa57523
arnold bwaila <arnold.bwaila@gmail.com>
2019-07-31 20:59:51 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,50 @@
+def using_push (myarray, mystring)
+  myarray.push(mystring)
+end
+
+def using_unshift (myarray, mystring3)
+    myarray.unshift(mystring3)
+end
+  
+
+def using_pop (myarray)
+  myarray.pop
+end
+
+def pop_with_args(myarray)
+  myarray.pop(2)
+end
+
+def using_shift(myarray)
+  myarray.shift
+end
+
+def shift_with_args(myarray)
+  myarray.shift(2)
+end
+
+def using_concat(myarray1, myarray2)
+  myarray1.concat(myarray2)
+end
+
+def using_insert(myarray, myelement)
+  myarray.insert(4, myelement)
+end
+
+def using_uniq(myarray)
+  myarray.uniq
+end
+
+def using_flatten(myarray)
+  myarray.flatten
+end
+
+def using_delete(myarray, mystring)
+  myarray.delete(mystring)
+end 
+
+def using_delete_at(myarray, myindex)
+  myarray.delete_at(myindex)
+end
+
+
