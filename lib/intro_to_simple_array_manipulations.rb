6d185d277bf76ec2b4eb76b22d103760b4fbf7e8
Chaya Deaver <chayadeaver@gmail.com>
2019-07-31 22:59:47 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,3 +4,33 @@ end
 def using_unshift(bouroughs_in_nyc, new_neighborhood )
   bouroughs_in_nyc.unshift(new_neighborhood)
 end
+def using_pop(continents)
+  continents.pop
+end
+def pop_with_args(dog_breeds)
+  dog_breeds.pop(2)
+end
+def using_shift(my_favorite_cities)
+  my_favorite_cities.shift
+end
+def shift_with_args(ice_cream_brands)
+  ice_cream_brands.shift(2)
+end
+def using_concat(my_favorite_things, all_my_favs)
+  my_favorite_things.concat(all_my_favs)
+end
+def using_insert(list_of_programming_languages, another_language)
+  list_of_programming_languages.insert(4, another_language)
+end
+def using_uniq(haircuts)
+  haircuts.uniq
+end
+def using_flatten(instruments)
+  instruments.flatten
+end
+def using_delete(instructors, no_offense_steven)
+  instructors.delete(no_offense_steven)
+end
+def using_delete_at(famous_robots, deleted_robot)
+  famous_robots.delete_at(deleted_robot)
+end
