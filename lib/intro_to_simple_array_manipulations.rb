b4d76efb9fa655eb463cd587558e6e31f1772b2b
Chaya Deaver <chayadeaver@gmail.com>
2019-07-31 20:13:17 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,6 @@
+def using_push(colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow.push(next_color)
+end
+def using_unshift(bouroughs_in_nyc, new_neighborhood )
+  bouroughs_in_nyc.unshift(new_neighborhood)
+end
