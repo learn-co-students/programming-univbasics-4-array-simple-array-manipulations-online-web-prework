8390f1642e6e151fab362c69ef00192b1eaef13a
Srikalyan Megati <kalmegati@gmail.com>
2019-08-01 14:44:10 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,57 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+
+def using_pop(array)
+  array.pop
+end
+
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+
+def using_shift(array)
+  array.shift
+end
+
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+
+def using_concat(con, cat)
+  con.concat(cat)
+end
+
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+
+def using_uniq(array)
+  array.uniq
+end
+
+
+def using_flatten(array)
+  array.flatten
+end
+
+
+def using_delete(array, element)
+  array.delete(element)
+end
+
+def using_delete_at(array, position)
+  array.delete_at(position)
+end 
