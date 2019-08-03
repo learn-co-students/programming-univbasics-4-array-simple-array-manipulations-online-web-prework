9d5702d2c498f73ead8711682525eec114936a13
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-08-03 23:09:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,6 +6,6 @@ describe "using_push"
   it "takes in two argument,an array and a string and adds that string to the end of the array using the push method"
     colors_in_the_rainbow = ["red", "orange", "yellow", "blue", "blue", "indigo"]
     next_color = "violet"
-    updated_array = using_push(colors_in_the_rainbow, next_color)
+    updated_array = using_push (colors_in_the_rainbow, next_color)
     
     
