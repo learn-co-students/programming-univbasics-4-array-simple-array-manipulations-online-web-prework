41f2f206fd02ebae82e549e47bf4862b0fe0d98b
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-08-03 23:02:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,2 +1,7 @@
 "pry"
-'./lib'
+'./lib/intro_to_simple_array_manipulations.rb'
+
+
+describe "using_push"
+  it "takes in two argument,an array and a string and adds that string to the end of the array using the push method"
+  
