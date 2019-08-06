adbfe1362a1e4bc4bad517b28b97929004b9b3a6
Brianna Jordan <bryvjordan9@gmail.com>
2019-08-06 03:00:27 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+def colors_in_the_rainbow
+  ["red","orange", "yellow", "green", "blue", "indigo"]
+end
+
+def using_push (array, next_color)
+  array.push("violet")
+end
+
+def using_unshift (array, new_neighborhood)
+  array.unshift("Staten Island")
+end
+
+def using_pop (array, continents)
+  array.pop("Antartica")
+end
+
+def using_size (array, continents)
+continents.size
+end
