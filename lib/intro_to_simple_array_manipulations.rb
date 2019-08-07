e8bccf6a474e293a844ec23732231373f557ac1b
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-08-07 20:53:52 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -28,9 +28,29 @@ def shift_with_args(array)
   brands_removed = ice_cream_brands.shift(2)
 end
 
-def using_concat(arrays!)
+def using_concat(my_favorite_things,more_favs)
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
   all_my_favs = my_favorite_things.concat(more_favs)
 end
+
+def using_insert(array,element)
+  list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+  another_language = "Python"
+  new_array = list_of_programming_languages.insert(3, another_language)
+  end
   
+def using_uniq(haircuts)
+  haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+  new_array = haircuts.uniq
+end
+
+def using_flatten(array)
+  instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
+  flat_array = instruments.flatten
+end
+
+def using_delete(array,string)
+  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+  no_offense_steven = instructors.delete("Steven")
+end
