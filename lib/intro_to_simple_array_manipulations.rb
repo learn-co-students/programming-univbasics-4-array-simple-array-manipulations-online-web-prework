440be078a3366fcf4f75668fd857b6c78dfcaa45
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-08-08 01:40:47 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,17 +10,17 @@ def using_unshift(array,string)
 end
 def using_pop(array)
   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  continents.pop
+  deleted_string = array.pop
 end
 
 def pop_with_args(array)
   dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
-    small_dogs = dog_breeds.pop(2)
+    small_dogs = array.pop(2)
 end 
 
 def using_shift(array)
   my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  im_so_over_this_city = my_favorite_cities.shift
+  im_so_over_this_city = array.shift
   end
   
 def shift_with_args(array)
@@ -28,16 +28,16 @@ def shift_with_args(array)
   brands_removed = ice_cream_brands.shift(2)
 end
 
-def using_concat(my_favorite_things,more_favs)
+def using_concat(array1,array2)
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_favs = ["sports cars", "flatiron school"]
-  all_my_favs = my_favorite_things.concat(more_favs)
+  all_my_favs = array1.concat(array2)
 end
 
 def using_insert(array,element)
   list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
   another_language = "Python"
-  new_array = list_of_programming_languages.insert(3, another_language)
+  new_array = list_of_programming_languages.insert(4, another_language)
   end
   
 def using_uniq(haircuts)
@@ -52,5 +52,11 @@ end
 
 def using_delete(array,string)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  no_offense_steven = instructors.delete("Steven")
+  string = "Steven"
+  no_offense_steven = array.delete(string)
+end
+
+def using_delete_at(array,integer)
+  famous_robots = ["Johnny 5", "R2D2", "Robocop"]
+  deleted_robot = famous_robots.delete_at(2)
 end
