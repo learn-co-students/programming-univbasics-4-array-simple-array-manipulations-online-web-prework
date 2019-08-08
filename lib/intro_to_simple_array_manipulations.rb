bdc1a1ea79ba434c73eb1938635dc5a9a62f6b0f
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 07:09:19 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,34 @@
+def "using_push" do
+  colors_in_the_rainbow.push (next_color)
+
+end
+
+def "using_unshift" do
+  bouroughs_in_nyc.unshift "Staten Island"
+
+end
+
+def "using_pop" do
+  @continents.pop
+
+end
+
+def "pop_with_args" do
+  @small_dogs = @dog_breeds.pop
+
+@small_dogs
+
+end
+
+def "using_shift" do
+  lagos =  @my_favorite_cities.shift
+
+end
+
+
+def
+  blue_bell_creameries = @ice_cream_brands.shift
+
+Blue Bell Creameries
+
+end
