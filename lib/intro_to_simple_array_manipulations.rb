7c6ab360264c69f3f7a64f8d187134da87ba4124
cedric e goddard <8ceg6@comcast.net>
2019-08-15 19:17:27 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,65 @@
+def using_push(colors_in_the_rainbow,next_color)
+    colors_in_the_rainbow.push(next_color)
+end
+
+
+def using_unshift(bouroughs_in_nyc,new_neighborhood)
+    bouroughs_in_nyc.unshift(new_neighborhood)
+    
+end  
+
+
+def using_pop(continents)
+    continents.pop
+    
+end
+
+def pop_with_args(dog_breeds)
+    dog_breeds.pop(2)
+end
+
+def using_shift(favorite_cities)
+    favorite_cities.shift
+end
+
+def shift_with_args(ice_cream_brands)
+    ice_cream_brands.shift(2)
+end    
+
+def using_concat(favorite_things,more_favs)
+    favorite_things.concat(more_favs)
+end    
+
+def using_insert(list_of_programming_languages, another_language)
+    another_language= "Python"
+    list_of_programming_languages.insert(4,another_language)
+    
+end
+
+def using_uniq(haircuts)
+    haircuts.uniq
+end
+
+
+
+def using_flatten(instruments)
+    instruments.flatten
+    
+end
+
+def using_delete(instructors,element_to_delete)
+    
+    instructors.delete("Steven")
+    
+end
+
+def using_delete_at(famous_robots,element_t_delete)
+    famous_robots.delete_at(2)
+
+
+end
+
+
+
+
+
