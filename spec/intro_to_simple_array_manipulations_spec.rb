5d07e98c95eb615ff0cae2ae4e48b7e9c5a3d368
Justin Miller <jayman1294@gmail.com>
2019-08-24 23:38:16 +0000

    Done.

--- spec/intro_to_simple_array_manipulations_spec.rb
+++ spec/intro_to_simple_array_manipulations_spec.rb
@@ -136,7 +136,7 @@ describe "using_delete" do
   it "takes in two arguments, an array and a string, and uses the delete method to remove any items from the array that are equal to that string" do
     instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
     no_offense_steven = using_delete(instructors, "Steven")
-    expect(instructors).not_to include("Steven")
+    expect(instructors).not_to include("steven")
   end
 end
 
