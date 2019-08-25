69fcdc82b8035b7bd98dd8f058c970ce1747893a
Jalysa Reason <jalysareason@gmail.com>
2019-08-25 14:05:03 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,10 @@
-array = ["1","2","3","4"]
-array = "5"
-array.push
+famous_cats=["lil bub","grumpy cat","maru"]
+=>["lil bub","grumpy cat","maru"]
+famous_cats
+
+famous_cats=["lil bub","grumpy cat","maru"]
+=>["lil bub","grumpy cat","maru"]
+famous_cats << "Nala cat"
+=>["lil bub","grumpy cat","maru","Nala cat"]
+famous_cats.push("Garfield")
+=>["lil bub","grumpy cat","maru","Nala cat","Garfield"]
