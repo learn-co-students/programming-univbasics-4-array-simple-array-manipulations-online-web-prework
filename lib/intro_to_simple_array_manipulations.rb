3873eda376203382526a8a794f3306abf5f17046
Maynard Hartman <mjhartman@brighthouse.com>
2019-08-28 08:32:22 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,36 @@
+def using_push(array, string)
+  array.push(string)
+END
+def using_unshift(array, string)
+  array.unshift(string)
+END
+def using_pop(array)
+  array.pop
+END
+def pop_witb_args(array, number)
+  array.pop(number)
+END
+def using_shift(array)
+  array.shift
+END
+def shift_with_args(array)
+  array.shift(2)
+END
+def using_concat(array1, array2)
+  array1.concat(array2)
+END
+def using_insert(array, element)
+  array.insert(element)
+END
+def using_uniq(array)
+  array.uniq
+END
+def using_flatten(array)
+  array.flatten
+END
+def using_delete(array, string)
+  arrray.delete(string)
+END
+def using_delete_at(array, index)
+  array.delete(index)
+END
