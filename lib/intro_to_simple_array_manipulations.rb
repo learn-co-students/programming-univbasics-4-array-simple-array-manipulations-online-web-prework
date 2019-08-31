fe71b3645972f7dfd182d87158b13a472133e5e7
Ruel Tingling <rvtingling@yahoo.com>
2019-08-31 09:52:35 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,9 @@
+def artist = [" nickleback", "ariana", "usher", "dmx"]
+  next_artist = "jay-z"
+  artist.push(next_artist)
+end 
+
+def artist = [" nickleback", "ariana", "usher", "dmx"]
+  next_artist = "jay-z"
+  artist.pop
+end 
