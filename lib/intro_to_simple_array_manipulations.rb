97c2387d221ac34b9fec492bf05b2e413cf57f45
Artiium Arbyummi <artiium21@gmail.com>
2019-09-02 18:57:57 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,51 @@
+def using_push(array,element) 
+  array.push(element) 
+  end
+ 
+ def using_unshift(bouroughs_in_nyc,new_neighborhood)
+   bouroughs_in_nyc.unshift(new_neighborhood)
+end    
+
+def using_pop(continents)
+  continents.pop
+end
+
+def pop_with_args(dog_breeds)
+  dog_breeds.pop(2)
+end
+
+def using_shift(my_favorite_cities)
+  my_favorite_cities.shift
+end
+
+def shift_with_args(family_members)
+  family_members.shift(2)
+end
+
+def using_concat(favorite_song,favorite_artist)
+  favorite_song.concat(favorite_artist)
+end
+
+def using_insert(a,b)
+  a.insert(4, b)
+end
+
+def using_uniq(zaheer)
+  zaheer.uniq
+end
+
+def using_flatten(artiium)
+  artiium.flatten
+end
+
+def using_delete(students,teachers)
+  students.delete(teachers) 
+end
+
+def using_delete_at(cheese,pork)
+  cheese.delete_at(2)
+end
+
+  
+    
+    
