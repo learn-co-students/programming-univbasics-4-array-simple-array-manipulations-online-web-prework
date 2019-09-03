91fee9f1430208156246a0ca9e86c9d0111f5e98
avid-enhancer-8592 <asma_asfak@yahoo.com>
2019-09-03 03:38:20 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,15 @@
+def using_push (colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift (bouroughs_in_nyc, new_neighborhood)
+  bouroughs_in_nyc.unshift(new_neighborhood)
+end
+
+def using_pop (continents)
+  continents.pop
+end
+
+def using_pop_with_args (dog_breeds)
+  dog_breeds.pop(2)
+end
