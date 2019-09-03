a4b4456d49ae4505abe990ed393d3cb03dacdb32
avid-enhancer-8592 <asma_asfak@yahoo.com>
2019-09-03 04:00:13 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,6 +10,39 @@ def using_pop (continents)
   continents.pop
 end
 
-def using_pop_with_args (dog_breeds)
+def pop_with_args (dog_breeds)
   dog_breeds.pop(2)
 end
+
+def using_shift (my_favorite_cities)
+  my_favorite_cities.shift
+end
+
+def shift_with_args (ice_cream_brands)
+  ice_cream_brands.shift(2)
+end
+
+def using_concat (my_favorite_things, more_favs)
+  my_favorite_things.concat(more_favs)
+end
+
+def using_insert (list_of_programming_languages, another_language)
+  list_of_programming_languages.insert(4, another_language)
+end
+
+def using_uniq (haircuts)
+  haircuts.uniq
+end
+
+def using_flatten (instruments)
+  instruments.flatten
+end
+
+def using_delete (instructors, name)
+  instructors.delete("Steven")
+end
+
+def using_delete_at (famous_robots, the_index)
+  famous_robots.delete_at(the_index)
+end
+
