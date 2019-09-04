247fad1b04e3a5ca73c0fa16118be9f2e89fad1d
Ray Reyes <rayreyes5@gmail.com>
2019-09-04 01:30:33 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,22 @@
+array.push(elements)
+ updated_array = using_push(colors_in_the_rainbow, next_color
+array.unshift
+ @updated_array = using_unshift(bouroughs_in_nyc, @new_neighborhood)
+array.pop
+@updated_array = using_unshift(bouroughs_in_nyc, @new_neighborhood)
+array.pop_with_args
+ @small_dogs = pop_with_args(@dog_breeds)
+array.shift
+ @im_so_over_this_city = using_shift(@my_favorite_cities)
+array.shift_with_args
+ @brands_removed = shift_with_args(@ice_cream_brands)
+array.concat
+@all_my_favs = using_concat(@my_favorite_things, more_favs)
+array.insert
+ @new_array = using_insert(@list_of_programming_languages, @another_language)
+array.uniq
+new_array = using_uniq(haircuts)
+array.delete 
+ no_offense_steven = using_delete(instructors, "Steven")
+array.delete_at
+ deleted_robot = using_delete_at(famous_robots, 2)
