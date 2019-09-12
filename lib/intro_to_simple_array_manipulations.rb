dda4bf530e43e8e2364933a1a11a7f2efa79a01c
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-09-12 21:40:32 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,57 @@
+def using_push(array, string)
+   updated_array= array.push(string)
+ end 
+ 
+ def using_unshift(array,string)
+   updated_array=array.unshift(string)
+ end 
+ 
+ def using_pop(array)
+  deleted_string=array.using_pop
+end 
+
+def pop_with_args(array,string)
+  return array.pop(string)
+end 
+
+def using_shift(array)
+   return array.shift 
+end 
+
+def shift_with_args(array,string)
+  updated_array=arry.shift(string)
+end 
+
+def 
+
+def using_concat(array1,array2)
+   return array1.concat(array2) 
+end
+
+def using_insert(array,string)
+  return array.insert(string) 
+end 
+
+def using_uniq(array)
+  return array.uniq 
+end 
+
+def using_flatten(array)
+  return array.flatten 
+end 
+
+def using_delete(array,string)
+  array=array.delete(string)
+end 
+
+def using_delete_at( array,index)
+  array = array.delete_at(index)
+end 
+
+
+
+
+
+
+
+
