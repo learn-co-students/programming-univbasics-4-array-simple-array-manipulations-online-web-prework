670a11c2816be3e0d7edd9d4505a45e3342c9684
Dwight Marie <dwightmarie@gmail.com>
2019-09-14 18:42:39 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+
+def using_push(array, string)
+ array = ["red", "orange", "yellow", "green", "blue", "indigo"]
+    word = string
+    array.push(word)
+end
+
+def using_unshift()
+
+using_pop()
+
+pop_with_args
+
+using_shift
+
+shift_with_args
+
+using_concat
+
+using_insert
+
+using_uniq
+
+using_flatten
+
+using_delete
+
+using_delete_at
