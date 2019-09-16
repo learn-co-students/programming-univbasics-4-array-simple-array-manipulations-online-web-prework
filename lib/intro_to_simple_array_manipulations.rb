9a68cb4970316126a368d93ef7241820c5f233c1
Tariq Bailey <tariqbailey627@gmail.com>
2019-09-16 03:56:06 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(array, string)
+array.push(string)
+end
+
+def using_unshift(array, string)
+array.unshift(string)
+end
+
+def using_pop(array)
+array.pop
+end
+
+def pop_with_args(array)
+array.pop(2)
+end
+
+def using_shift(array)
+array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_contact(array1, array2)
+array1.concat(array2)
+end
+
+def using_insert(array)
+array.insert(5)
+end
+
+using_unique(array)
+array.unique
+end
+
+using_flatten(array)
+array.flatten
+end
+
+using_delete(array, string)
+array.delete(string)
+end
+
+using_delete_at(array, integar)
+array.delete_at(integar)
+end
