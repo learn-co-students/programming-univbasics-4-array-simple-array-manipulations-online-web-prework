87b32d6aaae598267a7b091ee081a0cf866950a4
Joseph Miller <jdeshaiseworldtv@gmail.com>
2019-09-18 00:58:52 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,36 @@
+def artists ["Beyonce" , "kid rock" , "lil flip"]
+
+  artists.uning.pop ["kid rock"]
+
+
+def House_pets ["dog" ,"cat" , "fish"]
+
+  def House_pets.push ["hamster"]
+
+  end
+
+end
+
+def Animals ["worlf", "cow", "pig","horse"]
+
+  Animals.using_unshift ("horse")
+
+end
+
+def artists ["Beyonce" , "kid rock" , "lil flip"]
+
+  artists.uning.pop ["kid rock"]
+
+def artists.pop_with_args ["lil flip" , "kid rock"]
+
+array_artist #=> ["Beyonce"]
+
+end
+
+def artists ["Beyonce" , "kid rock" , "lil flip"]
+
+  artists.using_shift ["Beyonce"]
+
+  array #=> ("kid rock" "lil flip")
+
+end
