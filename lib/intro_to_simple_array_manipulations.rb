b1cc0fa4cd3cd3ed1b260fe3df03acee61818c93
Michael Waldo <mbwcogic@yahoo.com>
2019-09-19 03:44:30 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,10 @@
+require "pry"
+def using_push(rainbow_array, next_color)
+#binding.pry
+rainbow_array.push(next_color)
+end
+
+def using_unshift(boros, new_boro)
+  
+  boros.unshift(new_boro)
+end
