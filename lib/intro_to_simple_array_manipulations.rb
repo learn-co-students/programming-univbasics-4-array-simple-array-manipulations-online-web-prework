3cc3fd4db5f9b63b9853e962a8344d326207f435
Laura Hatch <laura.hatch.lh@gmail.com>
2019-09-19 20:37:35 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,26 +1,48 @@
 
 def using_push (array,string)
   array.push(string)
-  p array
 end
 
-def using_unshift (array,string)
+def using_unshift(array,string)
   array.unshift(string)
-  p array
 end
 
 def using_pop(array) # defined method
-   array_remove = array.pop # took element out of array
-  p array # print new array
-  p array_remove # print without last element
+   array.pop # took element out of array
 end
 
 def pop_with_args(array) #defined methods
-  pop_with_args.pop # removes last element of  array
-  p pop_with_args.pop # print array last element removed
-  p array
-def pop_with_args(array)
-  pop_with_args.pop# remove last one second time
-  p pop_with_args# calls method
-  p array # prints out finished array with last two removed
+  array.pop(2) # removes last element of  array
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(arrayone,arraytwo)
+arrayone.concat(arraytwo)
+end
+
+def using_insert(argone,element)
+  argone.insert(4,element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
+def using_delete_at(array,integer)
+  array.delete_at(integer)
 end
