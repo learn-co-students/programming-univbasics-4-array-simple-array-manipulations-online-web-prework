7eb9496dd46a4b687881908e803c61216ddf954d
Ryan Young <young.ryan1979@gmail.com>
2019-09-23 19:53:15 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,55 @@
+def using_push(colors_in_a_rainbow, next_color)
+  colors_in_a_rainbow.push(next_color)
+ 
+end
+def using_unshift(boroughs_in_nyc, new_neighborhood)
+  boroughs_in_nyc.unshift(new_neighborhood)
+
+end
+
+def using_pop(continents)
+  continents.pop
+
+end
+def pop_with_args(dog_breeds)
+  dog_breeds.pop(2)
+
+end
+
+def using_shift(the_thing_i_want_to_shift)
+  the_thing_i_want_to_shift.shift
+  
+end
+
+def shift_with_args(ice_cream_brands)
+  ice_cream_brands.shift(2)
+
+end
+
+def using_concat(favorite_things, other_things)
+  favorite_things.concat(other_things)
+end
+
+def using_insert(array, item)
+  array.insert(4, item)
+
+end
+
+def using_uniq(haircuts)
+  haircuts.uniq
+
+end
+
+def using_flatten (array)
+  array.flatten
+end
+
+def using_delete (array, string)
+  array.delete(string)
+end
+
+
+def using_delete_at (instructors, steven)
+  instructors.delete_at(steven)
+end
+  
