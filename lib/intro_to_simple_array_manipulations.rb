e19cced8a8bdbc14ebf6ac5522d90ddea01a02da
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-09-24 21:20:20 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,71 @@
+array = ["Hozier", "Ariana Grande", "Usher"]
+string = "Freida"
+
+def using_push(array, string)
+  array.push(string)
+end
+
+string = "Pablo Picasso"
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+array = ["Hozier", "Ariana Grande", "Usher"]
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+ array2 = ["Jameson", "Nikolai", "Nathan"]
+def using_concat(array, array2)
+  array.concat(array2)
+end
+element = "Naomi"
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+string = "Naomi"
+def using_delete(array, string)
+  array.delete(string)
+end
+
+element = 1
+def using_delete_at(array, element)
+  array.delete_at(element)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
