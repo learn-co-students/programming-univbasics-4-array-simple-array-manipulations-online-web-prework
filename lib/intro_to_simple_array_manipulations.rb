60d3360f15826188b02b62fcda743063acfc4811
Jonathan Norman <jnorman2123@gmail.com>
2019-09-25 17:08:41 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,47 +1,55 @@
-def using_push(colors_in_the_rainbow, next_color)
-  colors_in_the_rainbow.push(next_color) 
+def using_push(array, string)
+  array.push(string)
 end 
 
-def using_unshift(bouroughs_in_nyc, new_neighborhood)
-  bouroughs_in_nyc.unshift(new_neighborhood)
+
+def using_unshift(array, string)
+  array.unshift(string)
 end 
 
-def using_pop(continents)
-  continents.pop
-end
-
-def pop_with_args(dog_breeds)
-  dog_breeds.pop(2)
+def using_pop(array)
+  array.pop 
 end 
 
-def using_shift(my_favorite_cities)
-  my_favorite_cities.shift 
+def pop_with_args(array)
+  array.pop(2)
 end 
 
-def shift_with_args(ice_cream_brands)
-  ice_cream_brands.shift(2)
-end
-
-def using_concat(my_favorite_things, more_favs)
-  my_favorite_things.concat(more_favs)
+def using_shift(array)
+  array.shift 
 end 
 
-def using_insert(list_of_programming_languages, another_language)
-  list_of_programming_languages.insert(4, another_language) 
+def shift_with_args(array)
+  array.shift(2)
 end 
 
-def using_uniq(haircuts)
-  haircuts.uniq 
+def using_concat(array, array2)
+  array.concat(array2)
 end 
 
-def using_flatten(instruments)
-  instruments.flatten 
+def using_insert(array, n)
+  array.insert(4, n) 
 end 
 
-def using_delete(instructors, string )
-  instructors.delete(string)
+def using_uniq(array)
+  array.uniq 
 end 
 
-def using_delete_at(famous_robots, integer)
-  famous_robots.delete_at(integer)
+def using_flatten(array)
+  array.flatten 
 end 
+
+def using_delete(array, string)
+  array.delete(string)
+end 
+
+def using_delete_at(array, i)
+  array.delete_at(i)
+end 
+
+
+
+
+
+
+
