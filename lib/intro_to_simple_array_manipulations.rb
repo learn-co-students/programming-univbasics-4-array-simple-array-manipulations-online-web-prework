1c6e54f27137e7e5d20c1850efa0ae2db0ee17e6
Hoko Le <hokole527@gmail.com>
2019-09-25 17:45:28 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,5 +22,26 @@ def shift_with_args(array)
   array.shift(2)
 end
 
-def using_concat(array)
-  array.concat
+def using_concat(array, array2)
+  array.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq 
+end
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(2)
+end
