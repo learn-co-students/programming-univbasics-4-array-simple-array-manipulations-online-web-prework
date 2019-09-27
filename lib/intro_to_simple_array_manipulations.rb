5114fc6b592e5fdbd02b0eb695949e95fe0a57c7
allison cortez <allizetorc@gmail.com>
2019-09-27 16:54:34 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,48 @@
+def using_push
+  array.push
+end
+
+def using_unshift
+  array.unshift
+end
+
+def using_pop
+  array.pop
+end
+
+def pop_with_args
+  array.pop(2)
+end
+
+def using_shift
+  array.shift
+end
+
+def shift_with_args
+  array.shift(2)
+end
+
+def using_concat
+  array.concat(@my_favorite_things,@all_my_faves)
+end
+
+def using_insert
+  array.insert(4)
+end
+
+def using_uniq
+  array.uniq
+end
+
+def using_flatten
+  array.flatten
+end
+
+def using_delete(array,"Steven")
+  array.delete
+end
+
+def using_delete_at(array,2)
+  array.delete(2)
+end
+
