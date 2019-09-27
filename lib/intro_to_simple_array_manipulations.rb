d88f22b4511240ac27b22857d0dd4abd1d83d322
Nicole M Darby <nicoledarby021322@gmail.com>
2019-09-27 15:49:07 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,45 @@
+ artists=["Hozier","Ariana Grande","Usher"]
+ next_artist="Beyonce"
+ artist.push(next_artist)
+end
+
+ artist=["Frida Kahlo","Pablo Picasso","Cai Guo-Qiang"]
+ artist.pop
+ artist#=> 
+
+ artist=["Frida Kahlo","Pablo Picasso","Cai Guo-Qiang"]
+ artist.push"Andy Warhol"
+end
+ 
+ artist=["Frida Kahlo","Pablo Picasso","Cai Guo-Qiang"]
+ artist.unshift"Jasper Jones"
+ end
+ 
+ artist=["Frida Kahlo","Pablo Picasso","Cai Guo-Qiang"]
+ ("Pablo Picasso","Cai Guo-Qiang")
+ end
+ 
+ artist=["Frida Kahlo","Pable Picasso","Cai Guo-Qiang"]
+ artist.shift"Frida Kahlo"
+ end
+ 
+ artist=["Frida Kahlo","Pable Picasso","Cai Guo-Qiang"]
+ artist.concat
+ end
+ 
+ artist=["Frida Kahlo","Pable Picasso","Cai Guo-Qiang"]
+ artist.insert
+ end
+ 
+ artist=["Frida Kahlo","Pable Picasso","Cai Guo-Qiang"]
+ artist.uniq
+ end
+ artist=["Frida Kahlo","Pable Picasso","Cai Guo-Qiang"]
+ artist.flatten
+end
+
+ artist=["Frida Kahlo","Pable Picasso","Cai Guo-Qiang"]
+ artist.delete
+end
+ artist=["Frida Kahlo","Pable Picasso","Cai Guo-Qiang"]
+ artist.delete_via_redirect
