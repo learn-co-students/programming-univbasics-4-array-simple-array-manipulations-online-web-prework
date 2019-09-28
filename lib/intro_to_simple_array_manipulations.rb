85a3b6a9671fc952ac0e6bbc48f4e827df46f918
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-09-28 15:16:10 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+def using_push
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+    next_color = "violet"
+    colors_in_the_rainbow.push(next_color)
+  end
+def using_unshift
+ while  
+ bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+    new_neighborhood = "Staten Island"
+    do
+    bouroughs_in_nyc.unshift(new_neighborhood)
+  end
+def using_pop
+    continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+    continents.pop 
+  end
+
+  
