358873bfbf6d1ee9ede0d6e3d127924aaee7cd3a
Makeda P Mckune <makedamckune@gmail.com>
2019-09-30 06:57:06 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+
+colors_in_the_rainbow.push("violet")
+
+p colors_in_the_rainbow
+
+end
+
+
+
+bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+
+bouroughs_in_nyc.unshift("Staten Island")
+
+p bouroughs_in_nyc
+   
+end
+  
