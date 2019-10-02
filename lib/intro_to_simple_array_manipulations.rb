5c2b96e2d8fad1873b42d9de5b092fdf1bd8c1a4
Alianah Marie Magalued <alianahmagalued@yahoo.com>
2019-10-02 08:30:00 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_push(array) do
+  dog_names = ["bella", "kona", "misty", "mochi", "fergie", "lulu"]
+  next_name = "meme"
+  next_name.push
+end
+
+def using_unshift(array) do
+   dog_names = ["bella", "kona", "misty", "mochi", "fergie", "lulu"]
+  next_name = "meme"
+  next_name.unshift
+end
+
+def using_pop(array) do 
+  dog_names = ["bella", "kona", "misty", "mochi", "fergie", "lulu"]
+  dog_names.pop 
+end
