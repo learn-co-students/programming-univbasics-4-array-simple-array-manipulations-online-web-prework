af3d548338be9031bda65acd9a4d18e4d1f45019
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-03 18:38:39 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,55 @@
+my_loves = ['rokhaya','shahid','fama'.]
+new_love = 'ruby'
+my_loves.push(new_love)
+
+
+all_language = [ 'python', 'java','hanna']
+new_language = 'shift'
+all_language.unshift(new_language)
+
+
+be_happy = ['eat' , 'exercise' , 'laugh' ]
+more_happy = be_happy.pop
+p more_happy
+
+be_happy = ['eat' , 'exercise' , 'laugh' ]
+be_happy_happy.pop 
+
+happiness = ['pray ','hustle', 'slay']
+motivation = ['purpose' ,' determination' ,'abnegation']
+happiness[-1].concat << motivation[-1].concat
+
+goals =['determination' , 'will' , 'work' , 'patience']
+goals.insert(4 ,'consistency')
+
+
+fam_r = ['a','b','b','c','d','e','f']
+fam_r.uniq
+
+fam_r1 = ['a','b','c','d','e','f']
+fam_r1.flatten
+
+
+
+fam_r1 = ['a','b','c','d','e','f']
+fam_r1.delete('c')
+p fam_r1
+
+fam_r1 = ['a','b','c','d','e','f']
+fam_r1.delete( 2 ,'b')
+p fam_r1
+
+fam_r1 = ['a','b','c','d','e','f']
+fam_r1.delete('c')
+p fam_r1
+
+
+
+
+
+
+
+
+
+
+
