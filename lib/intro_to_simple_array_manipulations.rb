1426599a27f9532b76f0db01d24d00a67763a8fd
relational-router-4343 <ajagunnaabiodun752@yahoo.com>
2019-10-05 21:38:02 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,41 @@
+def using_push(array,string)
+  return array.push('violet')
+end
+def using_unshift(array,string)
+  return array.unshift('Staten Island')
+end
+
+def using_pop(array)
+   return array.pop
+  
+end
+def pop_with_args(array)
+  return array.pop(2)
+end
+def using_shift(array)
+   return array.shift
+end
+def shift_with_args(array)
+   return array.shift(2)
+end
+def using_concat(arr1,arr2)
+   return arr1.concat arr2
+    
+end
+def using_insert(array,element)
+  return array.insert(4,element)
+end
+def using_uniq(array)
+  return array.uniq
+end
+def using_flatten(array)
+   return array.flatten
+end
+def using_delete(array,string)
+   return array.delete('Steven')
+end
+def using_delete_at(array,interger)
+    return array.delete('Robocop')
+    
+end
+  
