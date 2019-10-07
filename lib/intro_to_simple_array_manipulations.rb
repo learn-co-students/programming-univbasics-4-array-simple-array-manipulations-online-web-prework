afa7af90af7b4631acad691fb711c602e0bc169d
Jeremy Bourne <jbourne25@yahoo.com>
2019-10-07 17:44:52 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,14 @@
+def using_push(colors_in_the_rainbow, next_color)
+  next_color = "violet"
+  colors_in_the_rainbow.push("violet")
+end
+
+def using_unshift(bouroughs_in_nyc, new_neighborhood)
+  new_neighborhood = "Staten Island"
+  bouroughs_in_nyc.unshift("Staten Island")
+end
+
+def using_pop(continents, deleted_string)
+  deleted_string = "Antarctica"
+  continents.pop("Antarctica") 
+  end
