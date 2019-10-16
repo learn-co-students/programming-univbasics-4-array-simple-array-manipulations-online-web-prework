b62c91257b4f504a40fc70bbd00cd7cac9795bd4
Melissa Holt <melissa.holt21@gmail.com>
2019-10-16 02:40:26 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,67 +1,47 @@
 def using_push(array, string)
-  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-  colors_in_the_rainbow.push("violet")
+  array.push(string)
 end
 
 def using_unshift(array, string)
-  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-  @new_neighborhood = "Staten Island"
-  bouroughs_in_nyc.unshift("Staten Island")
+  array.unshift(string)
 end
 
 def using_pop(array)
-  @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  @deleted_string = @continents.pop
+  array.pop
 end
 
 def pop_with_args(array)
-  @dog_breeds = ["Great Dane", "Golden Retrievers", "Chihuahua", "Shiba Inu"]
-  @small_dogs = ["Chihuahua", "Shiba Inu"]
-  @dog_breeds.pop(2)
+  array.pop(2)
 end
 
 def using_shift(array)
-  @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  @im_so_over_this_city = "Lagos"
-  @im_so_over_this_city = @my_favorite_cities.shift
+  array.shift
 end
 
 def shift_with_args(array)
-  @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  @brands_removed = ["Blue Bell Creameries", "Ben & Jerry's"] 
-  @ice_cream_brands.shift(2)
+  array.shift(2)
 end
 
-def using_push(array, string)
-  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-  colors_in_the_rainbow.push("violet")
+def using_concat(array1, array2)
+  array1.concat(array2)
 end
 
-def using_unshift(array, string)
-  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-  @new_neighborhood = "Staten Island"
-  bouroughs_in_nyc.unshift("Staten Island")
+def using_insert(array, element)
+  array.insert(4, element)
 end
 
-def using_pop(array)
-  @continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  @deleted_string = @continents.pop
+def using_uniq(array)
+  array.uniq
 end
 
-def pop_with_args(array)
-  @dog_breeds = ["Great Dane", "Golden Retrievers", "Chihuahua", "Shiba Inu"]
-  @small_dogs = ["Chihuahua", "Shiba Inu"]
-  @dog_breeds.pop(2)
+def using_flatten(array)
+  array.flatten
 end
 
-def using_shift(array)
-  @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  @im_so_over_this_city = "Lagos"
-  @im_so_over_this_city = @my_favorite_cities.shift
+def using_delete(array, string)
+  array.delete(string)
 end
 
-def shift_with_args(array)
-  @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  @brands_removed = ["Blue Bell Creameries", "Ben & Jerry's"] 
-  @ice_cream_brands.shift(2)
+def using_delete_at(array, integer)
+  array.delete_at(integer)
 end
