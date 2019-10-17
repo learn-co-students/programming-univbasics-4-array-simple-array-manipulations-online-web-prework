c1839ec42f3c39069db5570a0bb14898fba29342
Michael Calkins <m.calkins576@gmail.com>
2019-10-17 16:02:24 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,68 @@
+def using_push(array, new_item)
+  array.push(new_item)
+end
+
+def using_unshift(array, new_item)
+  array.unshift(new_item)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array, new_item)
+  array.concat(new_item)
+end
+
+def using_insert(array, new_item)
+  array.insert(4, new_item)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, remove_item)
+  array.delete(remove_item)
+end
+
+def using_delete_at(array, index)
+  array.delete_at(index)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
