095d638faa6515d9e9cabfa024d050529f214da9
jerrard suell <seulljay@gmail.com>
2019-10-18 16:17:05 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+def using_push(array, string)
+  artists = ["Hozier", "Jay", "Big"]
+  next_artist = "violet"
+  artists.push(next_artist)
+end
+
+def using_unshift(array, string)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  new_neighborhood = "Staten Island"
+  bouroughs_in_nyc.unshift(new_neighborhood)
+end
+
+def using_pop(array)
+  continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  Antarctica_continent = continents.pop
+  Antarctica_continent
+end
+
