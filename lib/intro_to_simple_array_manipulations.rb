315e1a53acb4f1f4c8f9a146096ebd2f8766a5ef
Zitai Huang <mywpiedu@gmail.com>
2019-10-22 02:02:48 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,8 +1,53 @@
-def using_push(Aray1,Str1)
-  Aray1.push(Str1)
+def using_push(exmar,example1)
+  exmar.push(example1)
 end
 
 
-def using_unshift(Array1,Str1)
-  Array1.unshift(Str1)
+def using_unshift(exmar,example1)
+  exmar.unshift(example1)
+end 
+
+
+def using_pop(exmar)
+  exmar.pop
 end
+
+def pop_with_args(exmar)
+  exmar.pop(2)
+  
+end
+
+def using_shift(exmar)
+  exmar.shift 
+end
+
+def shift_with_args(exmar)
+  exmar.shift(2)
+end
+
+def using_concat(exmar,exmar2)
+  exmar.concat(exmar2)
+end
+
+def using_insert(exmar,element)
+  exmar.insert(4,element)
+end
+
+def using_uniq(exmar)
+  exmar.uniq 
+end
+
+
+def using_flatten(exmar)
+  exmar.flatten 
+end
+
+def using_delete(exmar,element)
+  exmar.delete(element)
+end
+
+def using_delete_at(exmar,int1)
+  exmar.delete_at(int1)
+end
+
+  
