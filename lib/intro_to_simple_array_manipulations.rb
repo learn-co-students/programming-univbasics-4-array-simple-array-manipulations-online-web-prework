af33b7e30db0513878c703be943cf2cabb7f3f9e
Henry Phan <h.phan06@gmail.com>
2019-10-28 04:32:50 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,6 +42,6 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete_at(array, integer)
-  array.delete("Robocop")
+def using_delete_at(array, int)
+  array.delete_at(int)
 end
