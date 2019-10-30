5bedda882cbde25902224ea38a37dc2e8dd0079d
outstanding-reference-3248 <omar.benjamin377@gmail.com>
2019-10-30 15:57:13 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,35 @@
+def using_push
+array =
+end
+
+def using_unshift
+  array.unshift
+end
+
+def using_pop
+array.pop
+end
+
+def using_concat
+  array.concat
+end
+
+def using_insert
+  array.insert
+end
+
+def using_uniq
+  array.uniq
+end
+
+def using_flatten
+  array.flatten
+end
+
+def using_delete
+  array.delete
+end
+
+def using_delete_at
+  array
+end
