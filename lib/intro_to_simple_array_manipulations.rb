3be72015c3ef62da90b5fe2944f144d04d7a87b2
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-11-01 00:04:42 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,3 +33,15 @@ end
 def using_uniq(array)
   array.uniq
 end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(index = integer)
+end
