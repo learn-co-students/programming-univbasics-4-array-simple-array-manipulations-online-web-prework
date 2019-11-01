5615a20f406dbf055c213838b00368ac35f8eab2
dwane richards <dwanerich@gmail.com>
2019-11-01 22:33:42 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,55 @@
+def using_push(array, string)
+  new_ray = string
+  array.push(new_ray)
+end
+
+def using_unshift(array, string)
+  new_ray = string
+  array.unshift(new_ray)
+end
+
+def using_pop(array)
+  p array.pop
+end
+
+def pop_with_args(array, 2)
+  p array.pop[2]
+end
+
+def using_shift(array)
+  p array.shift
+end
+
+def shift_with_args(array, 2)
+  p array.shift[2]
+end
+
+def using_concat(ray1, ray2)
+  p ray2 = (ray1 + ray2).concat
+end
+
+def using_insert(array, element)
+  p array.insert[4] = element
+end
+
+def using_uniq(array)
+  p array.uniq
+end
+
+def using_flatten(array)
+  
+  
+end
+
+def using_delete(array, string)
+    
+end
+
+def using_delete_at(array, 2)
+  
+end
+
+
+  
+
+  
