93066a5d97b6a140143f47173d0cbe5c8777fe2d
Kelly Small <xcxkelly98@gmail.com>
2019-11-04 06:26:27 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,6 @@
+artists = ["Hozier", "Ariana Grande", "Usher"]
+next_artist = "Beyonce"
+artists.push["Usher"]
+
+artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+artists.pop['Cai Guo-Qiang']
