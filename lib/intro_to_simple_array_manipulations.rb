c9f89f370d9d1539e76ccfb1ed742d097dbe8f67
Brendan R Borgers <brendanborgers@gmail.com>
2019-11-21 20:06:31 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,50 @@
+def using_push(array, string)
+ array.push("colors_in_the_rainbow", "violet")
+end
+
+def using_unshift(array, string)
+  array.unshift("Staten Island")
+end
+
+def using_pop(continents)
+  continents.pop
+  
+end
+
+def pop_with_args(dog_breeds)
+  dog_breeds.pop(2)
+end
+
+def using_shift(my_favorite_cities)
+  my_favorite_cities.shift
+end
+
+def shift_with_args(ice_cream_brands)
+ice_cream_brands.shift(2)
+end
+
+def using_concat(my_favorite_things, more_favs) 
+  all_my_faves = my_favorite_things.concat(more_favs)
+end
+
+def using_insert(list_of_programming_languages, another_language)
+  new_array = list_of_programming_languages.insert(4, another_language)
+end
+  
+def using_uniq(haircuts)
+  haircuts.uniq
+end
+
+def using_flatten(instruments)
+  instruments.flatten
+end
+
+def using_delete(instuctors, no_offense_steven)
+  instuctors.delete("Steven")
+end
+  
+  def using_delete_at(famous_robots, deleted_robot)
+   famous_robots =  famous_robots.delete_at(2)
+    end
+  
+  
