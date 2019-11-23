38f45e27f06f600142fa459ac2a58f8410e78251
bionic-chain-9657 <mywisdom3@yahoo.com>
2019-11-23 23:21:39 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,62 @@
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+next_color = "violet"
+colors_in_the_rainbow.push(next_color)
+end
+
+bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+@new_neighborhood = "Staten Island"
+bouroughs_in_nyc.unshift(@new_neighborhood)
+end
+
+@continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+@continents.pop
+end
+
+@dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahu"]
+@dog_breeds.pop_with_args
+end
+
+ @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+ @my_favorite_cities.shift(@my_favorite_cities)
+ end
+ 
+   @ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+   @ice_cream_brands.shift_with_args
+ end
+ 
+   @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+    more_favs = ["sports cars", "flatiron school"]
+    @all_my_favorit_things.concat(more_favs)
+  end
+  
+    @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+    @another_language = "Python"
+    @list_of_programming_languages.insert(@another_language)[4]
+  end
+  
+  haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+  new_array = using_uniq(haircuts)
+  haircuts.uniq(new_array)
+  end
+  
+  instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
+  instruments.flatten(instruments)
+end
+
+ instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+ no_offense_steven = "Steven"
+ instructors.delete(no_offense_steven)
+ end
+ 
+ famous_robots = ["Johnny 5", "R2D2", "Robocop"]
+ deleted_robot = [2]
+ famous_robots.delete_at(deleted_robot)
+ end
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
