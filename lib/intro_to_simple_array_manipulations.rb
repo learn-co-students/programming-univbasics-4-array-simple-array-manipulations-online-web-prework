86e26fb12e54880aa83a269f7668a26ebc520e54
Jeffrey Martinez <martinezjf2@gmail.com>
2019-11-24 00:03:11 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,3 +4,22 @@
 def using_push(array, string)
   array.push("violet")
   end
+  
+  
+  
+def using_unshift(array, string)
+  array.unshift("Staten Island")
+  
+end
+
+
+
+def using_unshift(array, string)
+  
+end
+
+
+
+def using_unshift(array, string)
+  
+end
