c872e85309ee7c75b5058a63818100afa19a697a
Jeffrey Martinez <martinezjf2@gmail.com>
2019-11-25 05:39:19 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -43,9 +43,13 @@ def using_flatten(array)
   array.flatten
 end
 
+def using_delete(array, string)
+  array.delete(string)
+end
 
-
-
+def using_delete_at(array, index)
+  array.delete_at(index)
+end
 
 
 
