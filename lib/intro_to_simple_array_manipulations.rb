443f5d05d4cd9071f4e5c6c11f6d00330765a4fb
Caleb Willis <calebwillis50@yahoo.com>
2019-12-11 17:26:07 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_unshift(array)
+
+end
+
+def using_pop(array)
+  return
+end
+  
+def using_pop(array)
+  
+end
+
+def pop_with_args(array)
+  return
+end
+
+def pop_with_args(array)
+
+end
+
+def using_shift(array)
+  return
+end
+
+def using_shift(array)
+  
+end
+
+def shift_with_args(array)
+  return
+end
+
+def shift_with_args(array)
+  
+end
+
+def using_concat(array1, array2)
+  
+end
