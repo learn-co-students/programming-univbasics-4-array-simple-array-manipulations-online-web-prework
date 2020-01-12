24d27f9d7879b2a3beaef47270b6b2b6a6c2ffcf
beatific-media-1104 <riostechnologies@gmail.com>
2020-01-12 17:38:18 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,30 +1,29 @@
 artists = ["Hozier","Ariana Grande","Usher"]
 next_artist = "Beyonce"
-artist.push(next_artist)
+artists.push(next_artist)
 
 new_artist = "Bruno Mars"
 artists.unshift(new_artist)
 
 artists.pop
 
-artists.pop[-1,-2]
+artists.pop(1)
 
 artists.shift
 
-artists.shift[0,1]
+artists.shift(1)
 
 artists.concat 
 
-artists.insert[3]
+artists.insert(3)
 
 artists.uniq
 
 artists.flatten
 
-artists(new_artist)
-artists.delete
+artists.delete(artists)
 
-artists.delete[3]
+artists.delete(3)
 artists.uniq
 
 
