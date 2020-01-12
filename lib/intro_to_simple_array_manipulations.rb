1472deef95963bb7e2e9ce5e2757f343245b6811
beatific-media-1104 <riostechnologies@gmail.com>
2020-01-12 14:47:23 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,33 @@
+artists = ["Hozier","Ariana Grande","Usher"]
+next_artist = "Beyonce"
+artist.push(next_artist)
+
+new_artist = "Bruno Mars"
+artists.unshift(new_artist)
+
+artists.pop
+
+artists.pop[-1,-2]
+
+artists.shift
+
+artists.shift[0,1]
+
+artists.concat 
+
+artists.insert[3]
+
+artists.uniq
+
+artists.flatten
+
+artists(new_artist)
+artists.delete
+
+artists.delete[3]
+artists.uniq
+
+
+
+
+ 
