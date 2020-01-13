75612a980276b259410a214f44aa567cb8e15f97
Micah Banschick <micha.ban@gmail.com>
2020-01-13 19:20:54 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,51 @@
+def using_push(array,string)
+  array.push(string)
+end 
+
+def using_unshift(array,string)
+  array.unshift(string)
+end 
+
+def using_pop(array)
+  array.pop()
+end 
+
+def pop_with_args(array)
+  array.pop(2)
+end 
+
+def using_unshift(array,string)
+  array.unshift(string)
+end 
+
+def using_shift(array)
+  array.shift()
+end 
+
+def shift_with_args(array)
+  array.shift(2)
+end 
+
+def using_concat(array,arr2)
+  array.concat(arr2)
+end 
+
+def using_insert(array,str)
+  array.insert(4,str)
+end 
+
+def using_uniq(array)
+  arr = array.uniq()
+end 
+
+def using_flatten(array)
+  arr = array.flatten()
+end 
+
+def using_delete(array,string)
+  array.delete(string)
+end 
+
+def using_delete_at(array, int)
+  array.delete(array[int])
+end 
